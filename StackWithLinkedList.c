3fceb3d64325b537e27373d27a9b52025da4b85a
zcetin <46650409+zcetin@users.noreply.github.com>
2019-10-22 22:45:48 +0300

    Create StackWithLinkedList.c

---
+++ StackWithLinkedList.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+ struct n{
+ int data;
+ struct n* next;
+ };
+ typedef struct n node;
+
+void bastir(node*root){
+while(root!=NULL){
+        printf("%d ->",root->data);
+        root=root->next;
+        }
+}
+
+int pop(node *root){
+if(root==NULL){
+    printf("stack boss");
+    return -1;
+}
+if(root->next==NULL){
+    int rvalue=root->data;
+    free (root);
+    return rvalue;
+}
+node*iter=root;
+while(iter->next->next!=NULL)
+iter=iter->next;
+
+node *temp= iter->next;
+int rvalue=temp->data;
+iter->next=NULL;
+free(temp);
+
+return rvalue;
+}
+
+node* push(node *root,int a){
+if (root==NULL){
+    root=(node *)malloc(sizeof(node));
+    root->data=a;
+    root->next=NULL;
+    return root;
+}
+node *iter=root;
+while(iter->next!=NULL){
+    iter=iter->next;
+}
+node *temp=(node*)malloc(sizeof(node));
+temp->data=a;
+temp->next=NULL;
+iter->next=temp;
+return root;
+}
+
+int main()
+{
+    node*s=NULL;
+    s=push(s,10);
+    s=push(s,20);
+    printf("%d->",pop(s));
+    printf("%d->",pop(s));
+
+
+}
